123e9a88d0ddf145fdde6d4cab43c57241817841
busyrat <1435858847@qq.com>
2019-02-19 18:29:29 +0800

    hello: chrome-plugin

---
+++ hello-chrome-plugin/popup.js
@@ -0,0 +1,13 @@
+﻿
+var vip = document.getElementById('vip')
+var url = ''
+
+chrome.tabs.getSelected(null, function(tab){
+    setTimeout(function() {
+        url = tab.url
+    }, 0)
+})
+
+vip.addEventListener('click', function () {
+    window.open('http://app.baiyug.cn:2019/vip/index.php?url=' + url)
+})
